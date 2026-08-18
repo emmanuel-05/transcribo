a9ffb1d0e81d77e31c0f81b9b66a7f45b1e40c15
Gnamien Bi Hirigolin Emmanuel Perseverance <gnamien.em@gmail.com>
2026-08-18 00:20:44 +0000

    fix: update CORS configuration and add TrustedHostMiddleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -55,14 +55,24 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "http://localhost:3001",
         "http://127.0.0.1:3001",
-        "http://169.58.171.214:32801"
-        
+        "http://169.58.171.214:32801",
+        "https://transcribo.faslab.online"
     ],
     allow_origin_regex=r"http://(localhost|127\.0\.0\.1|10\.\d+\.\d+\.\d+|192\.168\.\d+\.\d+)(:\d+)?",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-    allowed_hosts=["169.58.171.214", "localhost", "127.0.0.1", "*"]
+)
+
+app.add_middleware(
+    TrustedHostMiddleware,
+    allowed_hosts=[
+        "faslab.online",
+        "*.faslab.online",
+        "169.58.171.214",
+        "localhost",
+        "127.0.0.1",
+    ],
 )
 
 # Inclusion des routeurs
